25daa8c937538c8ea679ac0dc3aa908def7ec14b
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2025-07-07 16:30:45 -0400

    when redirecting via dns the vite build base should be '/'

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
-    base: '/qts_1_1_0/',
+    base: '/',
     build: {
         outDir: 'build',
     },
